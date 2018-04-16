f534b5807ce51ec44474caf285bf5ef938c11054
Rafefix <36264913+Rafefix@users.noreply.github.com>
2018-04-17 00:46:55 +0200

    little shrimp up and down movement

--- Enemy_LittleShrimp.cpp
+++ Enemy_LittleShrimp.cpp
@@ -6,7 +6,7 @@ Enemy_LittleShrimp::Enemy_LittleShrimp(int x, int y) : Enemy(x, y)
 {
 	fly.PushBack({ 0,0,48,32 });
 	fly.PushBack({ 49,0,48,32 });
-	fly.speed = 0.01f;
+	fly.speed = 0.2f;
 
 	animation = &fly;
 
@@ -20,5 +20,21 @@ Enemy_LittleShrimp::Enemy_LittleShrimp(int x, int y) : Enemy(x, y)
 
 void Enemy_LittleShrimp::Move()
 {
-	position = original_position + path->GetCurrentPosition(&animation);
+	if (going_up)
+	{
+		if (wave > 1.0f)
+			going_up = false;
+		else
+			wave += 0.05f;
+	}
+	else
+	{
+		if (wave < -1.0f)
+			going_up = true;
+		else
+			wave -= 0.05f;
+	}
+
+	position.y = int(float(original_y) + (25.0f * sinf(wave)));
+	position.x -= 1;
 }
