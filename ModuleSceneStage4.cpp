dfc917cb1c56b98aaf399c8f32c0ffaa0592c4eb
Rafefix <36264913+Rafefix@users.noreply.github.com>
2018-06-04 17:32:31 +0200

    stage 4 activated

--- ModuleSceneStage4.cpp
+++ ModuleSceneStage4.cpp
@@ -61,6 +61,8 @@ bool ModuleSceneStage4::Start()
 	index2 = 0;
 	clearstage_fx = false;
 
+
+	App->player->enable_movement = true;
 	App->tentacles->Enable();
 	App->player->Enable();
 	App->enemies->Enable();
@@ -130,8 +132,8 @@ update_status ModuleSceneStage4::Update()
 		App->render->camera.x += 1 * SCREEN_SIZE;
 	}
 
-	App->render->Blit(back, 0, -50, &ground_top);
-	App->render->Blit(back, 0, 200, &ground_bottom);
+	App->render->Blit(back, 0, -50, &ground_top, 0.0f, true);
+	App->render->Blit(back, 0, 200, &ground_bottom, 0.0f, true);
 	App->render->Blit(graphics, 0, 0, &background);
 	App->render->Blit(hud, 0, 224, NULL, 0.0f, false);
 
