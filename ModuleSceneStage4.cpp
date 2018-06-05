362090d796f38800b6913aa3b4ed7be1b92369ba
FBarnes99 <ferranbarnes@gmail.com>
2018-06-05 13:44:43 +0200

    boss finally placed

--- ModuleSceneStage4.cpp
+++ ModuleSceneStage4.cpp
@@ -441,16 +441,17 @@ void ModuleSceneStage4::AddEnemies() {
 	App->enemies->AddEnemy(NEMONA_TENTACLE, 200, 200, -1, true);
 	App->enemies->AddEnemy(NEMONA_TENTACLE, 450, 200, -1, true);
 
-
+	
+	App->enemies->AddEnemy(BOSSHEART, 4905, 119, -1, true);
 	App->enemies->AddEnemy(BOSS1, 4839, 119, -1, true);
 	
 	//App->enemies->AddEnemy(BOSSARM, 4819, 127, -1, true);
 
 	App->enemies->AddEnemy(BOSSDISP, 4838, 119, -1, true);
-	App->enemies->AddEnemy(BOSSTENT, 4838, 119, -1, true);
+	App->enemies->AddEnemy(BOSSTENT, 4845, 26, -1, true);
 
-	//App->enemies->AddEnemy(BOSSHEART, 4905, 119, -1, true);
-	//App->enemies->AddEnemy(BOSSFACE, 4884, 119, -1, true);
+	
+	App->enemies->AddEnemy(BOSSFACE, 4884, 119, -1, true);
 
 
 }
